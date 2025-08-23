c164ad79e88fc15b2f00314ecba89070ab71b09e
Rishabh Singh Yadav <rishabhgoterror@gmail.com>
2025-08-23 16:54:58 +0530

    foe , while , do-while loops

---
+++ basics/loops.cpp
@@ -0,0 +1,33 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+
+    //for loop
+    // for(int i=1;i<10;i++){
+    //     cout<<"Rishabh"<<endl;
+    // }
+
+    //while loop
+    // int i=0;
+    // while (i<10)
+    // {
+    //     cout<<"Rishabh"<<endl;
+
+    //     i++;
+    // }
+    
+
+    // do while - a kind of while loop which at least runs once.
+    
+    int i=0;
+    do
+    {
+        cout<<"My name is rishabh"<<endl;
+        i++;
+    } while (i<10);
+    
+
+    return 0;
+}
+
