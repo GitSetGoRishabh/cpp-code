4ab7cc2a43bec470f89cc91418abe21a0740e12d
Rishabh Singh Yadav <rishabhgoterror@gmail.com>
2025-08-23 16:43:12 +0530

    arrays and strings

---
+++ basics/array-strings.cpp
@@ -0,0 +1,23 @@
+//array
+
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+    // int arr[3];
+
+    // cin>>arr[0]>>arr[1]>>arr[2];
+
+    // cout<<arr[1];
+
+    // //2D array
+
+    // int arr2[3][5];
+
+    //string
+
+    string name="Rishabh";
+    cout<<name[0]<<endl;
+    cout<<name.size();
+    return 0;
+}
