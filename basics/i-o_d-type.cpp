aed9e1518242931881e6ddc8e5543a2360b7268f
Rishabh Singh Yadav <rishabhgoterror@gmail.com>
2025-08-23 13:59:55 +0530

    Input/output , datatypes

---
+++ basics/i-o_d-type.cpp
@@ -0,0 +1,45 @@
+//basic skeleton and basic input and output
+
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+
+//     cout << "What is your age"<<endl;
+//     int age;
+//     cin >> age;
+
+
+
+
+// data types 
+// (numeric data type)
+//int , long , long long  (for integers)
+// float, double, long double (for decimal)
+// int x;
+// float y;
+// cin >> x;
+// cin >> y;
+
+// cout <<x<<endl<<y;
+
+//string or character data types - in double quotes for assigning value 
+// string , getline
+//string will take the characters until there is a space or newline.
+//getline will also take characters with space until there is a newline.
+
+// string str;
+// cin>>str;
+// cout<<str;
+// getline(cin,str);
+// cout<<str;
+
+//char for a single character , it takes less space than string so is preferred .It has to be in single quotes
+
+// char ch='r';
+// cout<<ch;
+
+
+
+return 0;
+}
