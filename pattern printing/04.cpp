ad8673c757ad67c3255c8221a4be4cf9d1347f5a
Rishabh Singh Yadav <rishabhgoterror@gmail.com>
2025-08-24 15:41:49 +0530

    pattern printing till 21

---
+++ pattern printing/04.cpp
@@ -0,0 +1,45 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+    int n=5;
+    int spaces=2*n-2;
+    // for(int i=1; i<=2*n-1;i++){
+    //     int stars=i;
+    //     if(i>n){
+    //         stars=2*n-i;
+    //     }
+    //     for(int j=1;j<=stars;j++){
+    //         cout<<"*";
+    //     }
+
+    //     for(int j=1;j<=spaces;j++){
+    //         cout<<" ";
+    //     }
+
+    //     for(int j=1;j<=stars;j++){
+    //         cout<<"*";
+    //     }
+    //     cout<<endl;
+    //     if(i<n){
+    //         spaces-=2;
+    //     }
+    //     else{
+    //         spaces+=2;
+    //     }
+    // } 
+
+    for(int i=0;i<n;i++){
+        for(int j=0;j<n;j++){
+            if(i==0 || j==0 || i==n-1 || j==n-1){
+                cout<<"*";
+            }
+            else{
+                cout<<" ";
+            }
+        }
+        cout<<endl;
+
+    }
+    return 0;
+}
