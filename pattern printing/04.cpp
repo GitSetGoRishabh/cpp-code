feab201de1aeb331cb32da72fa58d89af7617e24
Rishabh Singh Yadav <rishabhgoterror@gmail.com>
2025-08-24 20:54:09 +0530

    patern printing till 22

--- pattern printing/04.cpp
+++ pattern printing/04.cpp
@@ -2,8 +2,8 @@
 using namespace std;
 
 int main(){
-    int n=5;
-    int spaces=2*n-2;
+    // int n=5;
+    // int spaces=2*n-2;
     // for(int i=1; i<=2*n-1;i++){
     //     int stars=i;
     //     if(i>n){
@@ -29,17 +29,29 @@ int main(){
     //     }
     // } 
 
-    for(int i=0;i<n;i++){
-        for(int j=0;j<n;j++){
-            if(i==0 || j==0 || i==n-1 || j==n-1){
-                cout<<"*";
-            }
-            else{
-                cout<<" ";
-            }
+    // for(int i=0;i<n;i++){
+    //     for(int j=0;j<n;j++){
+    //         if(i==0 || j==0 || i==n-1 || j==n-1){
+    //             cout<<"*";
+    //         }
+    //         else{
+    //             cout<<" ";
+    //         }
+    //     }
+    //     cout<<endl;
+
+    // }
+    
+    int n=4;
+    for(int i=0;i<2*n-1;i++){
+        for(int j=0;j<2*n-1;j++){
+            int top = i;
+            int left = j;
+            int right = (2*n-2)-j;
+            int down = (2*n-2)-i;
+            cout<<(n-min(min(top,down),min(left,right)));
         }
         cout<<endl;
-
     }
     return 0;
 }
